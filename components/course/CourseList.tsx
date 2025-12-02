ff94b12edbcff6688fecaafb35b80b6b93d1e729
WildanAbdelF <wildanabdelf@gmail.com>
2025-12-02 19:10:39 +0700

    [FIX] Animation Bug at CourseCatalog

--- components/course/CourseList.tsx
+++ components/course/CourseList.tsx
@@ -3,12 +3,11 @@ import CourseCard from "./CourseCard";
 export default function CourseList({ courses }: { courses: any[] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {courses.map((c, index) => (
+      {courses.map((c) => (
         <div
           key={String(c.id)}
           data-aos="fade-up"
           data-aos-duration="500"
-          data-aos-delay={index * 80}
         >
           <CourseCard course={c} />
         </div>
